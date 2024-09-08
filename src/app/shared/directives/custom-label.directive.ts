669892d9cd15a513daa64a6feb9ede24798f7bf0
David <alv.ricardodavid@gmail.com>
2024-09-08 20:47:02 -0300

    Cambiar el mensaje de la etiqueta

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -7,9 +8,16 @@ export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
 
+  private _errors?: ValidationErrors | null;
+
   @Input() set color(value: string) {
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   constructor(private el: ElementRef<HTMLElement>) {
@@ -21,9 +29,38 @@ export class CustomLabelDirective implements OnInit {
     this.setStyle();
   }
 
-  setStyle():void {
-    if( !this.htmlElement )  return;
+  setStyle(): void {
+    if (!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color;
   }
 
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerHTML = 'No hay errores';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerHTML = 'Este campo es requerido';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      let requerido = this._errors['minlength']?.['requiredLength'];
+      let caracteres = this._errors['minlength']?.['actualLength'];
+      this.htmlElement.nativeElement.innerHTML = `Caracteres ingresados ${caracteres }, el minimo es ${requerido}`;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerHTML = 'No es un mail valido';
+      return;
+    }
+
+
+  }
 }

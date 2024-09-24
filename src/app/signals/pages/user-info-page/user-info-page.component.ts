9b903859c2636b955ff1a97521e1248fd95670fe
David <alv.ricardodavid@gmail.com>
2024-09-24 09:43:19 -0300

    Señales - Informacion del usuario

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
-  styleUrl: './user-info-page.component.css'
+  styleUrl: './user-info-page.component.css',
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  private usersServices = inject(UsersServiceService);
 
+  public userId = signal(1);
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number) {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+
+    this.currentUser.set(undefined);
+
+    this.usersServices.getUserById(id).subscribe((user) => {
+      this.currentUser.set(user);
+    });
+  }
 }
